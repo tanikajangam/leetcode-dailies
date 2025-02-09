26fa49dda824e58df7f6f1c4091f5c938857fecd
“Tanika <tanikajangam03@gmail.com>
2025-02-09 09:09:59 -0500

    feb 9th

---
+++ 2:9:2025/countBadPairs.py
@@ -0,0 +1,27 @@
+
+from typing import List
+
+
+class Solution:
+    def countBadPairs(self, nums: List[int]) -> int:
+        goodPairs = 0
+        freq = {}
+
+        for i in range(len(nums)):
+            diff = nums[i] - i  
+            if diff in freq:
+                goodPairs += freq[diff]
+                freq[diff] += 1
+            else:
+                freq[diff] = 1
+        
+        return (len(nums)*(len(nums)-1))//2-goodPairs
+            
+solution = Solution()
+
+
+nums = [4,1,3,3]
+print(solution.countBadPairs(nums))
+
+nums = [1,2,3,4,5]
+print(solution.countBadPairs(nums))
