ae1bcc8a0ddc96643afd09ec194644aba595a2fc
“Tanika <tanikajangam03@gmail.com>
2025-03-03 06:55:56 -0500

    march 3rd

--- March-2025/pivotArrays.py
+++ March-2025/pivotArrays.py
@@ -1,3 +1,32 @@
+from typing import List
+
+
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        smaller = []
+        larger = []
+        equal = []
         
+        for i in range(len(nums)):
+            if nums[i] < pivot:
+                smaller.append(nums[i])
+            
+            elif nums[i] > pivot:
+                larger.append(nums[i])
+            
+            else:
+                equal.append(nums[i])
+            
+        result = []
+        
+        result.extend(smaller)
+        result.extend(equal)
+        result.extend(larger)
+        return result
+    
+
+solution = Solution()
+nums = [9,12,5,10,14,3,10]
+pivot = 10
+
+# print(solution.pivotArray(nums, pivot))
